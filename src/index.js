49cb011ac1eefffbeb681659e6982e50bd885309
Aditya <adityajugran42@gmail.com>
2018-02-03 16:12:57 +0530

    listing of Todo app

--- src/index.js
+++ src/index.js
@@ -3,28 +3,28 @@ import ReactDOM from "react-dom";
 import data from "./data.json";
 import styles from "./index.css";
 
+
+
 class Todo extends React.Component {
   constructor() {
     super();
     this.state={
        data:data.data
     }
-    this.handleClick = this.handleClick.bind(this);
   }
-  handleClick(e) {
-    const change = e.status === ""?"complete":"";
-    e.status = change;
-    this.setState({
-      status:change
-    });
 
 
-  }
+
+
   render(){
     const list=this.state.data.map((data,id)=>
-    <li className="list-group-item" key={data.id}><input type="checkbox" onClick={(e) => this.handleClick(data, e)}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{data.text}<span className="badge">{data.status}</span></li>
+    <li className="list-group-item" key={data.id}>
+    <input type="checkbox" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    {data.text}<span className="badge"></span></li>
 
   );
+
     return(
       <div id="wrapper">
       <h1>TODO</h1>
