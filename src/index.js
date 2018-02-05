ac9456c08f6fc0e41c1d6b526d405792aa7f6944
Aditya <adityajugran42@gmail.com>
2018-02-05 12:28:27 +0530

    adding componetents to todo

--- src/index.js
+++ src/index.js
@@ -9,13 +9,31 @@ class Todo extends React.Component {
   constructor() {
     super();
     this.state={
-       data:data.data
+       data:data.data,
+       text: ''
     }
   }
 
-
-
-
+handleSubmit =(e) =>{
+  e.preventDefault();
+  if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      id: Date.now(),
+      text: this.state.text,
+      status:false
+    };
+    this.setState(prevState => ({
+      data: prevState.data.concat(newItem),
+      text: ''
+    }));
+    // console.log(this.state.data)
+}
+handleChange=(e)=> {
+  // console.log(e.target.value);
+    this.setState({ text: e.target.value });
+  }
   render(){
     const list=this.state.data.map((data,id)=>
     <li className="list-group-item" key={data.id}>
@@ -31,6 +49,11 @@ class Todo extends React.Component {
       <ul className="list-group">
       {list}
       </ul>
+      <br />
+      <input type="text" placeholder="Task Name" className="form-control" onChange={this.handleChange} value={this.state.text}/>
+      <br /><br />
+
+      <button className="buttonAlter btn btn-info" onClick={this.handleSubmit}> Add</button>
       </div>
     );
   }
